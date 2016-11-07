37386450bb786dc8c833ef79672f8ebc7471cae8
Shogo Kitazume <beaver.dubmaster@gmail.com>
2016-11-07 14:26:30 +0000

    Add the age column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,7 +21,6 @@ class MessagesController < ApplicationController
   end
   
   def create
-    binding.pry
     @message = Message.new(message_params)
       if @message.save
         redirect_to root_path , notice: 'メッセージを保存しました'
@@ -41,7 +40,7 @@ class MessagesController < ApplicationController
   private
   
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   
   def set_message
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031141648) do
+ActiveRecord::Schema.define(version: 20161107130901) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
